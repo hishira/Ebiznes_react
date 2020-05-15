e0d7943bb4a7081988fe786b74b9c791dc0da04c
michal <micha0125@o2.pl>
2020-05-15 19:08:15 +0200

    zadanie_6#5

--- src/Api/UserApi.js
+++ src/Api/UserApi.js
@@ -3,7 +3,14 @@ import {createFetchObject,createApiLink} from "./ApiConfig";
 async function authUser(obj) {
     var url = createApiLink("authuser")
     return fetch(url,createFetchObject(obj)).then(dane=>dane.json())
-
+}
+async function checkUserLogin(name){
+    var url = createApiLink('userexists')
+    return fetch(url,createFetchObject(name)).then(dane=>dane.json())
+}
+async function createUser(user){
+    var url = createApiLink('usercreatejson')
+    return fetch(url,createFetchObject(user))
 }
 
-export {authUser}
+export {authUser,checkUserLogin,createUser}
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,18 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-
+import {checkUserLogin,createUser} from "../Api/UserApi";
+import history from "../history";
 function Copyright() {
     return (
         <Typography variant="body2" color="textSecondary" align="center">
@@ -51,7 +49,26 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SignUp() {
     const classes = useStyles();
+    const [login,setLogin] = useState("");
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("");
+    const [rePassword,setRepassword] = useState("")
+    async function createHandle(event) {
+        event.preventDefault()
+        console.log(login,email,password,rePassword)
+        if(rePassword !== password){
+            alert("Hasla sie nie zgadzaja")
+        }
+        let count = -1
+        await checkUserLogin({login:login}).then(dane=>count = dane)
+        if(count >0){
+            alert("Uzytkownik o podanym login istnieje")
+        }else if(count === 0) {
+            await createUser({login:login,email:email,password:password})
+            history.push('/')
+        }
 
+    }
     return (
         <Container component="main" maxWidth="xs">
             <CssBaseline />
@@ -62,28 +79,30 @@ export default function SignUp() {
                 <Typography component="h1" variant="h5">
                     Sign up
                 </Typography>
-                <form className={classes.form} noValidate>
+                <form className={classes.form}  onSubmit={createHandle} autoComplete="off">
                     <TextField
                         variant="outlined"
                         margin="normal"
-                        required
+                        required={true}
                         fullWidth
                         id="email"
                         label="Email Address"
                         name="email"
-                        autoComplete="email"
                         autoFocus
+                        onChange={(event)=>setEmail(event.target.value)}
+                        required
                     />
                     <TextField
                         variant="outlined"
                         margin="normal"
-                        required
                         fullWidth
                         id="login"
                         label="Login"
                         name="login"
                         autoComplete="username"
                         autoFocus
+                        onChange={(event)=> setLogin(event.target.value)}
+                        required
                     />
                     <TextField
                         variant="outlined"
@@ -94,7 +113,20 @@ export default function SignUp() {
                         label="Password"
                         type="password"
                         id="password"
-                        autoComplete="current-password"
+                        required
+                        onChange={(event)=> setPassword(event.target.value)}
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="re-password"
+                        label="Re-Password"
+                        type="password"
+                        id="re-password"
+                        required
+                        onChange={(event)=>setRepassword(event.target.value)}
                     />
 
                     <Button
